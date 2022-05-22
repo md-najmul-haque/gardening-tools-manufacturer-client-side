13401e26fea095c07b465d79d4274a20dfed169a
najuml-bd <najmul0612@gmail.com>
2022-05-22 20:11:41 +0600

    simple login form added with react-hook-form

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
+
     return (
         <div class="card w-96 bg-base-100 shadow-xl flex justify-center">
             <div class="card-body">
-                <input type="text" placeholder="Type here" class="input input-bordered input-primary w-full max-w-xs" />
-                <input type="text" placeholder="Type here" class="input input-bordered input-secondary w-full max-w-xs" />
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Login</button>
-                </div>
+
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+
+                    <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" {...register("email", { required: true })} />
+
+                    {errors.name?.type === 'required' && "Your email is required"}
+
+                    <input type="password" placeholder="Type here" class="input input-bordered w-full max-w-xs" {...register("password", { required: true })} />
+                    {errors.password && "Your password is required"}
+
+                    <input type="submit" class="btn btn-primary" />
+                </form>
             </div>
         </div>
     );
