b274ba4e05b1c466b054697876b8e27f26f62baa
Md Najmul Haque <najmulhaque008@gmail.com>
2022-09-09 21:56:14 +0600

    review start added

--- src/Pages/Home/Review/Review.js
+++ src/Pages/Home/Review/Review.js
@@ -2,6 +2,8 @@ import React from 'react';
 import defaultUser from '../../../assets/user/defaultUser.png'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 const Review = ({ review }) => {
 
@@ -13,10 +15,29 @@ const Review = ({ review }) => {
             data-aos-duration="1000"
             className="card w-96 bg-base-100 shadow-2xl mb-5 mx-auto">
             <div className="card-body items-center text-center">
-                <p className="card-title"> <img style={{ width: 40 }} className='w-6/12' src={img ? img : defaultUser} alt="Shoes" className="rounded-xl" /> {name}</p>
-                <p>Review: {feedback}</p>
-                <p>Rating: {rating}</p>
-                <p>Country: {country}</p>
+                <img style={{ width: 80 }} className='w-6/12' src={img ? img : defaultUser} alt="customer-img" />
+                <p className="card-title rounded-xl">{name}</p>
+                <p className='pt-2 h-[120px]'>{feedback}</p>
+                <div className='flex'>
+                    {
+                        rating >= 1 ? (<FontAwesomeIcon className="text-orange-400" icon={faStar} />) : <FontAwesomeIcon className="text-orange-200" icon={faStar} />
+                    }
+
+                    {
+                        rating >= 2 ? (<FontAwesomeIcon className="text-orange-400" icon={faStar} />) : <FontAwesomeIcon className="text-orange-200" icon={faStar} />
+                    }
+                    {
+                        rating >= 3 ? (<FontAwesomeIcon className="text-orange-400" icon={faStar} />) : <FontAwesomeIcon className="text-orange-200" icon={faStar} />
+                    }
+                    {
+                        rating >= 4 ? (<FontAwesomeIcon className="text-orange-400" icon={faStar} />) : <FontAwesomeIcon className="text-orange-200" icon={faStar} />
+                    }
+                    {
+                        rating >= 5 ? (<FontAwesomeIcon className="text-orange-400" icon={faStar} />) : <FontAwesomeIcon className="text-orange-200" icon={faStar} />
+                    }
+
+                </div>
+                <p className=''>Country: {country}</p>
             </div>
         </div>
     );
