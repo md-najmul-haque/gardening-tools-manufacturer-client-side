88c1d04da5fc5b590e8c6e3e965bf7645a2d766b
Md Najmul Haque <najmulhaque008@gmail.com>
2022-09-16 17:52:51 +0600

    home page title size updated

--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -34,7 +34,7 @@ const Reviews = (props) => {
     return (
 
         <div id='testimonial' className='my-10 bg-gray-50 py-10' >
-            <h1 className='zoom-in text-4xl text-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-secondary to-primary mb-2'>TESTIMONIALS</h1>
+            <h1 className='zoom-in text-3xl text-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-secondary to-primary mb-2'>TESTIMONIALS</h1>
             <h2 className='text-4xl text-center mb-10'>Our Customers <span className='font-bold'>Are Saying?</span></h2>
 
             <Carousel
--- src/Pages/Home/Tools/Tools.js
+++ src/Pages/Home/Tools/Tools.js
@@ -8,7 +8,7 @@ const Tools = () => {
 
     return (
         <div id='products' className='py-10'>
-            <h2 className='text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-secondary to-primary mb-5 text-center'>Feature Products</h2>
+            <h2 className='text-3xl pb-3 font-bold text-transparent uppercase bg-clip-text bg-gradient-to-r from-secondary to-primary mb-5 text-center'>Feature Products</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto'>
                 {
                     products.map(product => <Tool key={product._id} product={product}></Tool>)
